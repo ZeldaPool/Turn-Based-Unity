45858c61cd86b69449a61bc431f46e44e26298c3
ZeldaPool <smitrana007@gmail.com>
2019-01-12 11:58:26 +0530

    First Upload
    Please Work....

---
+++ Turn Based First Attempt/Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseEnemy
+{
+
+    public string name;
+
+    //Next one for Enemy Types
+    public enum Type
+    {
+        Grass,
+        Fire,
+        Water,
+        Electric
+    }
+
+    public Type EnemyType;
+
+    public float baseHP;
+    public float curHP;
+    public float baseMP;
+    public float curMP;
+    public float baseATK;
+    public float cutATK;
+    public float baseDEF;
+    public float curDEF;
+
+
+}
