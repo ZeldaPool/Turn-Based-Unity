8135b81348247ba78ecdb75912ea1d7d5e28fc2c
ZeldaPool <smitrana007@gmail.com>
2019-01-20 14:17:24 +0530

    3rd one

---
+++ Turn Based First Attempt/Assets/Scripts/BattleStateMachine.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BattleStateMachine : MonoBehaviour {
+
+    public enum PerformAction
+    {
+
+        Wait,
+        TakeAction,
+        PerformAction
+    }
+
+    public PerformAction BattleStates;
+
+    public List<HandleTurns> PerformList = new List<HandleTurns>();
+
+    public List<GameObject> HerosInBattle = new List<GameObject>();
+
+    public List<GameObject> EnemiesInBattle = new List<GameObject>();
+
+
+	// Use this for initialization
+	void Start () {
+
+        BattleStates = PerformAction.Wait;
+        EnemiesInBattle.AddRange (GameObject.FindGameObjectsWithTag("Enemy"));
+        HerosInBattle.AddRange(GameObject.FindGameObjectsWithTag("Hero"));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        switch(BattleStates)
+        {
+
+            case (PerformAction.Wait):
+                if(PerformList.Count>0)
+                {
+                    BattleStates = PerformAction.TakeAction;
+                }
+
+                break;
+
+            case (PerformAction.TakeAction):
+                GameObject performer = GameObject.Find(PerformList[0].Attacker);
+                if(PerformList[0].type=="enemy")
+                {
+                    EnemyStateMachine ESM = performer.GetComponent<EnemyStateMachine>();
+                    ESM.HeroToAttack = PerformList[0].AttackersTarget;
+                    ESM.CurrentState = EnemyStateMachine.Turnstate.Action;
+
+                }
+
+                BattleStates = PerformAction.PerformAction;
+                
+
+                break;
+
+            case (PerformAction.PerformAction):
+
+                break;
+
+
+        }
+	
+        
+
+	}
+
+
+    public void CollectActions(HandleTurns input)
+    {
+       PerformList.Add(input);
+
+    }
+}
--- Turn Based First Attempt/Assets/Scripts/EnemyStateMachine.cs
+++ Turn Based First Attempt/Assets/Scripts/EnemyStateMachine.cs
@@ -4,15 +4,167 @@ using UnityEngine;
 
 public class EnemyStateMachine : MonoBehaviour {
 
+    private BattleStateMachine BSM;
+
     public BaseEnemy enemy;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public enum Turnstate
+    {
+        Processing,
+        ChooseAction,
+        Waiting,
+        Action,
+        Dead
+    }
+
+    public Turnstate CurrentState;
+    //For Processstate
+    private float cur_cooldown = 0f;
+    private float max_cooldown = 5f;
+    //This Gameobject
+    private Vector3 startpostition;
+
+    //timeforaction
+
+    private bool actionStarted = false;
+
+    public GameObject HeroToAttack;
+
+    private float animspeed = 5f;
+
+    
+
+
+    // Use this for initialization
+    void Start () {
+
+        CurrentState = Turnstate.Processing;
+        BSM = GameObject.Find("BattleManager").GetComponent<BattleStateMachine>();
+        startpostition = transform.position;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        Debug.Log(CurrentState);
+
+        switch (CurrentState)
+        {
+
+            case (Turnstate.Processing):
+
+                UpgradeProgressBar();
+
+
+                break;
+
+            case (Turnstate.ChooseAction):
+
+                ChooseAction();
+                CurrentState = Turnstate.Waiting;
+
+
+                break;
+
+
+
+            case (Turnstate.Waiting):
+
+
+                break;
+
+           
+
+            case (Turnstate.Action):
+
+                StartCoroutine(TimeForAction());
+
+
+                break;
+
+            case (Turnstate.Dead):
+
+
+                break;
+
+        }
+
+    }
+
+    void UpgradeProgressBar()
+    {
+        cur_cooldown = cur_cooldown + Time.deltaTime;
+        
+        if (cur_cooldown >= max_cooldown)
+        {
+            CurrentState = Turnstate.ChooseAction;
+        }
+
+    }
+
+    void ChooseAction()
+    {
+        HandleTurns myAttack = new HandleTurns();
+        myAttack.Attacker = enemy.name;
+        myAttack.type = "enemy";
+        myAttack.AttackGameObject = this.gameObject;
+        myAttack.AttackersTarget = BSM.HerosInBattle[Random.Range(0, BSM.HerosInBattle.Count)];
+        BSM.CollectActions(myAttack);
+       
+
+    }
+
+    private IEnumerator TimeForAction()
+    {
+        if (actionStarted)
+        {
+
+            yield break;
+
+        }
+
+        actionStarted = true;
+
+        //animate the enemy near the hero to attacck
+        Vector3 heroPosition = new Vector3 (HeroToAttack.transform.position.x-1.5f, HeroToAttack.transform.position.y, HeroToAttack.transform.position.z);
+        while(MoveTowardsEnemy(heroPosition))
+        {
+            yield return null;
+
+        }
+
+        //Wait a bit
+        yield return new WaitForSeconds(0.5f);
+
+        //do damage
+
+        //animate back to start position
+        Vector3 firstposition = startpostition;
+        while (MoveTowardsStart(firstposition))
+        {
+            yield return null;
+        }
+        //Remove performer from the list in BSM
+        BSM.PerformList.RemoveAt(0);
+
+        //Reset BSM= wait
+        BSM.BattleStates = BattleStateMachine.PerformAction.Wait;
+        actionStarted = false;
+        //reset this enemy state
+        cur_cooldown = 0f;
+        CurrentState = Turnstate.Processing;
+    }
+
+    private bool MoveTowardsEnemy(Vector3 target)
+    {
+        return target != (transform.position = Vector3.MoveTowards(transform.position, target, animspeed * Time.deltaTime));
+    }
+
+    private bool MoveTowardsStart(Vector3 target)
+    {
+        return target != (transform.position = Vector3.MoveTowards(transform.position, target, animspeed * Time.deltaTime));
+    }
+
 }
+
